c0eb82781bfda0416479ebd9597bd18e2eec8f46
pratibha-1 <pratibha.2902s@gmail.com>
2022-09-11 12:33:23 +0530

    Frontend with mysql connection

---
+++ CollaborativeEditingApplication/MySqlConnectionWithApp/server.js
@@ -0,0 +1,20 @@
+﻿require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const errorHandler = require('_middleware/error-handler');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.use('/users', require('./users/users.controller'));
+
+
+app.use(errorHandler);
+
+
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+app.listen(port, () => console.log('Server listening on port ' + port));
